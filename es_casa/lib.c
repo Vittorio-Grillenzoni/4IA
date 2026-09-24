b560a9243d8cb20822baddf2e4ef54c03d0a309e
Vittorio-Grillenzoni <vittorio.grillenzoni@galileiostiglia.edu.it>
2026-09-24 19:47:50 +0000

    b

--- es_casa/lib.c
+++ es_casa/lib.c
@@ -2,21 +2,19 @@ void identityMatrix(int i, int m[i][j]){
     for (int i = 0; i < DIM; i++) {
         for (int j = 0; j < DIM; j++) {
             if (i == j) {
-                matrice[i][j] = 1; 
+                m[i][j] = 1; 
             } else {
-                matrice[i][j] = 0; 
+                m[i][j] = 0; 
             }
         }
     }
     
-    // Stampa della matrice
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%d ", matrice[i][j]);
+            printf("%d ", m[i][j]);
         }
         printf("\n");
     }
     
     return 0;
-}
 }
